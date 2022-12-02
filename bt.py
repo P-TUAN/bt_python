667734bb162b107feee4be57bf2fc6352a167bc3
pham tuan <89393220+P-TUAN@users.noreply.github.com>
2022-12-02 14:15:08 +0700

    Update and rename bt.python to bt.py

---
+++ bt.py
@@ -0,0 +1,43 @@
+#bài tập 1
+def xuat():
+  t = int(input("Nhập: "))
+  if 0 < t <= 100:
+    for i in range (1,t+1):
+        str1=input()
+        nguyenam = 0
+        phuam = 0
+        str1.lower()
+        for n in str1:
+         if(n == 'a' or n == 'e' or n == 'i' or i == 'o' or n == 'u'):
+          nguyenam = nguyenam + 1
+        else:
+         phuam = phuam + 1
+         print(f"Test{i}:\n{nguyenam} \n{phuam}" )
+                    
+
+xuat()
+
+#bài tập 2
+def xuat():
+  t = int(input("Nhập: "))
+  if 0 < t <= 100:
+    for i in range (1,t+1):
+        str1=input()
+        nguyenam = 0
+        phuam = 0
+        str1.lower()
+        for n in str1:
+         if(n == 'a' or n == 'e' or n == 'i' or i == 'o' or n == 'u'):
+          nguyenam = nguyenam + 1
+        else:
+         phuam = phuam + 1
+         print(f"Test{i}:\n{nguyenam} \n{phuam}" )
+                    
+
+xuat()
+
+##bài tập 3
+
+          
+          
+         
